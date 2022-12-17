b9fe07c6e0c1d6415766d230360e3078cfa8d704
Matthew Salinger <42982273+mattsal13@users.noreply.github.com>
2022-12-17 13:54:46 -0500

    Create bj_game.py

---
+++ bj_game.py
@@ -0,0 +1,16 @@
+from bjfunctions import *
+
+num_decks = 4
+
+## Create the shoe. 4 * num_decks of each rank--suit doesn't matter. (NEED TO RENAME THIS SHOE)
+deck = dict()
+for i in range(2, 11):
+    deck[f'{i}'] = 4 * num_decks
+deck['J'] , deck['Q'], deck['K'], deck['A'] = 4 * num_decks, 4 * num_decks, 4 * num_decks, 4 * num_decks
+
+## See the bjfunctions file for all the functions that go into play_round(): this initiates a round of blackjack.
+while deck_count(deck) > 26:
+    play_round(deck)
+    # For space
+    for _ in range(3):
+        print('.')
